f2e7e4fbdd23942abb516078aac59156633a00d8
zzy96 <zhouzhiyaohflser@gmail.com>
2016-03-30 23:25:40 +0800

    distance.js fixed

--- distance.js
+++ distance.js
@@ -1,5 +1,5 @@
 function calculateDistance(userLon, userLat, taxiLon, taxiLat){
-	return Math.sqrt(((userLon-taxiLon)/360*40000)^2+((userLat-taxiLat)/180*40000)^2);
+	return Math.sqrt(Math.pow((userLon-taxiLon)/360*40000,2)+Math.pow((userLat-taxiLat)/360*40000,2));
 }
 
-console.log(calculateDistance(103.819836, 1.362083, 103.687089, 1.343828));
+console.log(calculateDistance(103.679384, 1.342022, 103.687089, 1.343828));
